c7279de86a80aa58f6b0707be74149a9b2caece8
João Vitor <vitorjoao.jv626@gmail.com>
2022-12-23 00:50:41 -0300

    Feito da parte do Create (crud) Desde o limpa, preenchimento, recolhimento de dados, e organização Com observação na data BR, na limpeza das abas fechamento da guia modal

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ var dados = []
 function populaTabela() {
   if (Array.isArray(dados)) {
 
+    localStorage.setItem("__dados__", JSON.stringify(dados))
+
     $("#tblDados tbody").html("")
 
     dados.forEach(function (item) {
@@ -20,10 +22,41 @@ function populaTabela() {
 
 $(function () {
   //EXECUTA AO CARREGAR A TELA
-  dados = JSON.parse(localStorage.getItem("--"))
+  dados = JSON.parse(localStorage.getItem("__dados__"))
 
   if (dados) {
     populaTabela()
   }
+
+  $("#btnSalvar").click(function()  {
+    //EVENTO CLICK DO BOTÃO SALVAR
+
+    let Nome = $("#txtNome").val()
+    let Sobrenome = $("#txtSobrenome").val()
+    let DtNascimento = new Date($("#txtDtNascimento").val()).toLocaleDateString("pt-br", { timeZone: "UTC" })
+    let Formacao = $("#txtFormacao").val()
+
+    let registro = {}
+
+    registro.Nome = Nome
+    registro.Sobrenome = Sobrenome
+    registro.DtNascimento = DtNascimento
+    registro.Formacao = Formacao
+
+    registro.ID = dados.length + 1;
+
+    dados.push(registro)
+
+    alert("Registro salvo com sucesso!")
+    $("#modalRegistro").modal("hide")
+
+    //LIMPEZA DE DADOS
+    $("#txtNome").val("")
+    $("#txtSobrenome").val("")
+    $("#txtDtNascimento").val("")
+    $("#txtFormacao").val("")
+
+    populaTabela()
+  })
 
 })
